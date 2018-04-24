c27865d6457a2bb0590f20c09d443ce84e86d74f
RGS-NAYAZ-LT\Nayaz <nayaz.h@lifetechnologyservices.com>
2018-04-24 19:20:17 +0530

    Added model storing UserFeedprofile and created migrations.

--- src/profile_project/profiles_api/models.py
+++ src/profile_project/profiles_api/models.py
@@ -62,3 +62,15 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         """Django uses this to when it needs convert the object to string"""
 
         return self.email
+
+class ProfileFeedItem(models.Model):
+    """Profile status update."""
+
+    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """returns the model as string."""
+
+        return self.status_text
