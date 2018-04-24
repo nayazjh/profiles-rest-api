3462df53944b0d87fc4efd66b87593c5239d0125
RGS-NAYAZ-LT\Nayaz <nayaz.h@lifetechnologyservices.com>
2018-04-24 18:03:41 +0530

    Added login api.

--- src/profile_project/profiles_api/serializers.py
+++ src/profile_project/profiles_api/serializers.py
@@ -22,7 +22,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
         user = models.UserProfile(
             email=validated_data['email'],
-            name=validated_data['name'],
+            name=validated_data['name']
         )
 
         user.set_password(validated_data['password'])
